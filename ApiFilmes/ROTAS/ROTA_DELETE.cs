29538e9cc2c45737cff498f5f39d63c889a3caaf
eloicesar <162243229+eloicesar@users.noreply.github.com>
2025-05-30 19:11:16 -0300

    Update ROTA_DELETE.cs

--- ApiFilmes/ROTAS/ROTA_DELETE.cs
+++ ApiFilmes/ROTAS/ROTA_DELETE.cs
@@ -2,16 +2,16 @@ public static class Rota_DELETE
 {
     public static void MapDeleteRoutes(this WebApplication app)
     {
-        // Mapeia a rota HTTP DELETE para remover um filme pelo ID
+      
         app.MapDelete("/api/filmes/{id}", async (int id, FilmesContext context) =>
         {
-            // Busca o filme pelo id no banco de dados
+   
             var filme = await context.Filmes.FindAsync(id);
             // Se não encontrar o filme, retorna 404 Not Found
             if (filme is null)
                 return Results.NotFound();
 
-            // Remove o filme do banco de dados
+            
             context.Filmes.Remove(filme);
             // Salva as alterações de forma assíncrona
             await context.SaveChangesAsync();
